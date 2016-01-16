c38de4d948f22686920c7c32d7f153cbb8e73dc2
MartinPlessDK <Martinplessdk@gmail.com>
2016-01-16 15:27:18 +0100

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/tradableapp/AccountController.java
@@ -0,0 +1,34 @@
+package com.tradableapp;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tradable.api.services.analytics.AccountAnalyticListener;
+import com.tradable.api.services.analytics.AccountMetricsUpdateEvent;
+import com.tradable.api.services.analytics.CurrentAccountAnalyticService;
+
+public class AccountController implements AccountAnalyticListener{
+
+	@Autowired
+	private CurrentAccountAnalyticService service;
+	
+	@PostConstruct
+    public void init() {
+        service.addAccountAnalyticListener(this);
+    }
+
+    @PreDestroy
+    public void unsubscribe() {
+        service.removeAccountAnalyticListener(this);
+    }
+	
+	@Override
+	public void accountMetricsChanged(AccountMetricsUpdateEvent event) {
+		System.out.println("Account metrics changed " + event);
+	}
+	
+	
+
+}
---
+++ src/main/java/com/tradableapp/TradableAppFactory.java
@@ -0,0 +1,48 @@
+package com.tradableapp;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tradable.api.entities.Account;
+import com.tradable.api.entities.AccountMetrics;
+import com.tradable.api.services.account.CurrentAccountService;
+import com.tradable.api.services.account.CurrentAccountServiceListener;
+import com.tradable.api.services.analytics.CurrentAccountAnalyticService;
+import com.tradable.api.services.marketdata.QuoteTickService;
+import com.tradable.ui.workspace.WorkspaceModule;
+import com.tradable.ui.workspace.WorkspaceModuleCategory;
+import com.tradable.ui.workspace.WorkspaceModuleFactory;
+
+public class TradableAppFactory implements WorkspaceModuleFactory {
+
+	@Autowired
+	protected QuoteTickService quoteTickService;
+	@Autowired
+	protected CurrentAccountService currentAccountService;
+	@Autowired
+	protected CurrentAccountAnalyticService currentAccountAnalyticService;
+	
+	@Override
+	public WorkspaceModule createModule() {
+		// TODO Auto-generated method stub
+		
+		return new TradableApp(quoteTickService, currentAccountService, currentAccountAnalyticService);
+	}
+
+	@Override
+	public WorkspaceModuleCategory getCategory() {
+		// TODO Auto-generated method stub
+		return WorkspaceModuleCategory.MISCELLANEOUS;
+	}
+
+	@Override
+	public String getDisplayName() {
+		// TODO Auto-generated method stub
+		return "FiskeFar Kommer efter dig!";
+	}
+
+	@Override
+	public String getFactoryId() {
+		// TODO Auto-generated method stub
+		return "com.tradableapp.FiskeFar";
+	}
+	
+}
